b3778425c70da2f5f62291e0b5977081a1b3b38a
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-16 22:18:04 +0500

    namedEntityRecognition

--- main.py
+++ main.py
@@ -9,4 +9,16 @@ text = [
     'elon musk is my favorite person',
     'the athlete was about to win the game'
 ]
-nlp = spacy.load('en_core_web_md')
+
+
+#nlp = spacy.load('en_core_web_md')
+#ner_labels = nlp.get_pipe('ner').labels
+#print(ner_labels)
+
+categories = ['ORG', 'PERSON', 'LOC']
+
+docs = [nlp(text) for text in texts]
+
+for doc in docs:
+    entities =[]
+    for ent in
