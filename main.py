0bd4c3ee6a530a3594e189abcd11521a4e2a4b2b
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-16 22:13:28 +0500

    namedEntityRecognition

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import spacy
+
+
+text = [
+    'John loves to go to shopping!',
+    'Jasra is palying a game',
+    'musa is not preparing for exam',
+    'ahmed is the guy behind the car'
+    'elon musk is my favorite person',
+    'the athlete was about to win the game'
+]
+nlp = spacy.load('en_core_web_md')
