1bd0f382143e07f63b33d92e66de213532dd1f0d
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-17 20:22:46 +0500

    namedEntityRecognition

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 import spacy
+import random
+from spacy.util import minibatch
+from spacy.training.example import Example
 
 
-text = [
-    'John loves to go to shopping!',
-    'Jasra is palying a game',
-    'musa is not preparing for exam',
-    'ahmed is the guy behind the car'
-    'elon musk is my favorite person',
-    'the athlete was about to win the game'
+
+train_data = [
+    ("What is the price of 10 bananas?? ", {"entities":[(21,23, "QUANTITY"),(24, 31, "PRODUCT")]}
+    
+    
+    
+    )
 ]
 
-
-#nlp = spacy.load('en_core_web_md')
-#ner_labels = nlp.get_pipe('ner').labels
-#print(ner_labels)
-
-categories = ['ORG', 'PERSON', 'LOC']
-
-docs = [nlp(text) for text in texts]
-
-for doc in docs:
-    entities =[]
-    for ent in
