7e89dcf416dced7b5db500070e6172d1f144bd7c
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-18 19:11:23 +0500

    namedEntityRecognition

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ train_data = [
 
 nlp = spacy.load('en_core_web_md')
 
-if 'ner' not nlp.pipe_names:
-    nlp.add_pipe('ner')
+if 'ner' not in nlp.pipe_names:
+    ner = nlp.add_pipe('ner')
 else:
-    nlp.get_pipe('ner')
+    ner = nlp.get_pipe('ner')
 
 
 for _,annotations in train_data:
@@ -54,7 +54,7 @@ with nlp.disable_pipes(*other_pipes):
         losses = {}
         batches = minibatch(train_data, size=2)
         for batch in batches:
-            example = []
+            examples = []
             for text, annotations in batch:
                 doc = nlp.make_doc(text)
                 example = Example.from_dict(doc, annotations)
@@ -62,3 +62,19 @@ with nlp.disable_pipes(*other_pipes):
             nlp.update(examples, drop=0.5, losses=losses)
 
         print(f'Epoch {epoch+1}, Losses: {losses}')
+
+
+nlp.to_disk('custom_ner_model')
+trained_nlp =spacy.load('custom_ner_model')
+
+test_texts = [
+    "How much for 3 oranges?",
+    "I Want 15 chairs for the conference.",
+    "Can you give me the price for 6 desks?"
+]
+
+for text in test_texts:
+    doc = trained_nlp(text)
+    print(f'Text:{text}')
+    print("Entities ", [(ent.text, ent.label_) for ent in doc.ents])
+    print()
